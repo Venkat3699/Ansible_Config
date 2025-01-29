dfce84286e27b0b420bf2ccd7ece22bd2e3f6669
venkat3699 <ravindrareddy.mukka9@gmail.com>
2025-01-29 15:21:48 +0530

    updated files

--- Ansible/security_groups.tf
+++ Ansible/security_groups.tf
@@ -5,15 +5,34 @@ resource "aws_security_group" "allow_tls_terraform" {
   vpc_id      = aws_vpc.vpc_terraform.id
 
   # Allowing ingress for allowed ports
-  dynamic "ingress" {
-    for_each = var.allowed_ports
-    content {
-      from_port   = ingress.value
-      to_port     = ingress.value
-      protocol    = "tcp"  # Ensure this is set to "tcp"
-      cidr_blocks = ["0.0.0.0/0"]  # Change this to your IP for production
-    }
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.1.0/32"]
+  }
+
+  ingress {
+    from_port   = 3389
+    to_port     = 3389
+    protocol    = "tcp"
+    cidr_blocks = ["10.1.1.0/32"]
+  }
+
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["10.2.1.0/32"]
+  }
+
 
   egress {
     from_port   = 0
