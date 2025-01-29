402108dc6ba40a8fad5f72b8d54a44116dce6e86
venkat3699 <ravindrareddy.mukka9@gmail.com>
2025-01-29 16:13:20 +0530

    updated files

--- Ansible/null-local-exec.tf
+++ Ansible/null-local-exec.tf
@@ -2,8 +2,8 @@ resource "null_resource" "webservers" {
   provisioner "local-exec" {
     command = <<EOH
       sleep 10
-      timeout 300 ansible -i invfile pub -m ping
+      ansible -i invfile pvt -m ping
     EOH
   }
-  depends_on = [local_file.ansible_inventory_file]
+  depends_on = [local_file.ansible-inventory-file]
 }
