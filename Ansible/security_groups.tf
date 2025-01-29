c0fa534ceb7b74721e883be7043baa83fa1fbea4
venkat3699 <ravindrareddy.mukka9@gmail.com>
2025-01-29 16:31:22 +0530

    updated files

--- Ansible/security_groups.tf
+++ Ansible/security_groups.tf
@@ -12,19 +12,19 @@ resource "aws_security_group" "allow_tls_terraform" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
-  }
+  # ingress {
+  #   from_port   = 80
+  #   to_port     = 80
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
+  # }
 
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from anywhere
-  }
+  # ingress {
+  #   from_port   = 443
+  #   to_port     = 443
+  #   protocol    = "tcp"
+  #   cidr_blocks = ["0.0.0.0/0"]  # Allow HTTPS from anywhere
+  # }
 
     egress {
     from_port   = 0
